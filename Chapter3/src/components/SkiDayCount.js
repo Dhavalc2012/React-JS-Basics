cd7f931eb14636809daacf82af6010612468258b
Dhaval <dhavalchheda@ymail.com>
2018-04-14 22:09:26 +0530

    Chapter 3 Additon of react icons

--- Chapter3/src/components/SkiDayCount.js
+++ Chapter3/src/components/SkiDayCount.js
@@ -1,4 +1,7 @@
 import '../stylesheets/ui.scss'
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calender from 'react-icons/lib/fa/calendar'
 
 
 const percentToDecimal = (decimal) => {
@@ -15,14 +18,17 @@ export const SkiDayCount = (props) => (
             <div className="ski-day-count">
                 <div className="total-days">
                     <span>{props.total}</span>
+                    <Calender/>
                     <span> days</span>
                 </div>
                 <div className="powder-days">
                     <span>{props.powder}</span>
+                    <SnowFlake/>
                     <span> days</span>
                 </div>
                 <div className="backcountry-days">
                     <span>{props.backcountry}</span>
+                    <Terrain/>
                     <span> hiking days</span>
                 </div>
                 <div>
