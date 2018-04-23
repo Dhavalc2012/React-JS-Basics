9e74e1cf8b8ae38e42048b4d87a5ebeb91bfc3ff
Dhaval <dhavalchheda@ymail.com>
2018-04-23 17:51:21 +0530

    Addition of the refactor of the React to the ES6

--- Chapter3/src/components/app.js
+++ Chapter3/src/components/app.js
@@ -1,38 +1,40 @@
-import { createClass} from 'react'
+import { Component} from 'react'
 import {SkiDayList} from "./SkiDayList";
 import {SkiDayCount} from "./SkiDayCount";
 
-export const App = createClass({
-    getInitialState(){
-            return {
-                allSkiDays : 		[
-                    {
-                        resort: "Squa Valley",
-                        date : new Date("1/2/2016"),
-                        powder: true,
-                        backcountry: false
-                    },
-                    {
-                        resort: "Timous Chapter",
-                        date : new Date("3/4/2016"),
-                        powder: false,
-                        backcountry: true
-                    },
-                    {
-                        resort: "Beyond Wine & Dine",
-                        date : new Date("7/4/2016"),
-                        powder: false,
-                        backcountry: false
-                    },
-                    {
-                        resort: "Ski Tampolon",
-                        date : new Date("1/22/2016"),
-                        powder: false,
-                        backcountry: true
-                    }
-                ]
-            }
-    },
+export class App extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            allSkiDays : 		[
+                {
+                    resort: "Squa Valley",
+                    date : new Date("1/2/2016"),
+                    powder: true,
+                    backcountry: false
+                },
+                {
+                    resort: "Timous Chapter",
+                    date : new Date("3/4/2016"),
+                    powder: false,
+                    backcountry: true
+                },
+                {
+                    resort: "Beyond Wine & Dine",
+                    date : new Date("7/4/2016"),
+                    powder: false,
+                    backcountry: false
+                },
+                {
+                    resort: "Ski Tampolon",
+                    date : new Date("1/22/2016"),
+                    powder: false,
+                    backcountry: true
+                }
+            ]
+        }
+
+    }
     countDays(filter){
         return this.state.allSkiDays.filter(function(day) {
                 if(filter){
@@ -41,7 +43,7 @@ export const App = createClass({
                     return day
                 }
         }).length
-    },
+    }
     render(){
         return(
             <div className="app">
@@ -52,4 +54,4 @@ export const App = createClass({
             </div>
         )
     }
-})
+}
