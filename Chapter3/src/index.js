fbafc18bcc783c12a7012786b5c06710c37bfea2
Dhaval <dhavalchheda@ymail.com>
2018-04-23 15:18:29 +0530

    Chapter 4-3 Props and States Displaying Child Component

--- Chapter3/src/index.js
+++ Chapter3/src/index.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount} from "./components/SkiDayCount";
+import { SkiDayList} from "./components/SkiDayList";
 window.React = React;
 
 render(
-	<SkiDayCount total={50}
-	powder={20}
-	backcountry={10}
-	goal={100}/>,
+	<SkiDayList days={
+		[
+			{
+				resort: "Squa Valley",
+				date : new Date("1/2/2016"),
+				powder: true,
+				backcountry: false
+			},
+            {
+                resort: "Timous Chapter",
+                date : new Date("3/4/2016"),
+                powder: false,
+                backcountry: true
+            },
+            {
+                resort: "Beyond Wine & Dine",
+                date : new Date("7/4/2016"),
+                powder: false,
+                backcountry: false
+            },
+            {
+                resort: "Ski Tampolon",
+                date : new Date("1/22/2016"),
+                powder: false,
+                backcountry: true
+            }
+		]
+	}/>,
 	document.getElementById('react-container')
 )
